692f3eae22cf7a75858a48ec71225c40a68f6517
Quyet114 <tquyet1998@gmail.com>
2023-11-24 00:58:11 +0700

    update: forgot password

--- controller/passwordController.js
+++ controller/passwordController.js
@@ -1,9 +1,10 @@
 
 const { User } = require('../models/User');
-const nodemailer=require('nodemailer')
-
+const nodemailer = require('nodemailer')
+const bcrypt = require('bcrypt');
 
 const passwordController = {
+
     sendMail: async (req, res, next) => {
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
@@ -11,25 +12,27 @@ const passwordController = {
             secure: true,
             auth: {
                 user: 'tquyet1998@gmail.com',
-                pass: 'yydzudzdfoiztqdf'  
+                pass: 'yydzudzdfoiztqdf'
                 //ok
             }
         });
+        const passwordNew = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000).toString();
         try {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(passwordNew, salt);
             const user = await User.findOne({ username: req.body.username })
             if (user) {
-                const passwordNew = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
-                console.log(passwordNew);
-                console.log();
-                transporter.sendMail({
+                await transporter.sendMail({
                     from: '"My gmail" <tquyet1998@gmail.com>',
                     to: user.email,
                     subject: "Yêu cầu mật khẩu mới",
                     text: `xin chào: ${user.username}`,
                     html: `Mật khẩu mới của bạn là : ${passwordNew}`
                 });
-                await User.updateOne({ email: user.email }, { $set: { password: passwordNew } })
-                res.status(200).json({message: "Gửi mail thành công" });
+                // await User.updateOne({email: user.email},{ $set: { password: passwordNew } });
+                await User.findOneAndUpdate({ email: user.email }, { $set: { password: hashedPassword } },
+                    { returnDocument: "after" });
+                res.status(200).json({ message: "Gửi mail thành công" });
             }
             else {
                 res.status(500).json({ status: err, message: "không tìm thấy email" });
